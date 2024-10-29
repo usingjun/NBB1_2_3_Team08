751d07b3c3bb35310eaa79c86846b44e3883c895
Si-rauis <kkyd7189@gmail.com>
2024-10-29 15:46:21 +0900

    commit

---
+++ Learner_Kotlin/src/main/kotlin/edu/example/learner_kotlin/config/JPAAuditConfig.java
@@ -0,0 +1,10 @@
+package edu.example.learner_kotlin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JPAAuditConfig {
+
+}
