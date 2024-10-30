82222a32fe7d31ea97ab59097327262e8b36ffd9
namhyeonh0 <www101365@naver.com>
2024-10-30 12:39:35 +0900

    Answer 관련 기능 완료

--- src/main/kotlin/edu/example/learner_kotlin/qna/inquiry/entity/Inquiry.kt
+++ src/main/kotlin/edu/example/learner_kotlin/qna/inquiry/entity/Inquiry.kt
@@ -1,6 +1,7 @@
 package edu.example.learner_kotlin.qna.inquiry.entity
 
 import edu.example.learner_kotlin.member.entity.Member
+import edu.example.learner_kotlin.qna.answer.entity.Answer
 import jakarta.persistence.*
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
@@ -29,5 +30,8 @@ data class Inquiry(
 
     @ManyToOne @JoinColumn(name = "member_id")
     var member: Member? = null,
+
+    @OneToOne(mappedBy = "inquiry", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var answer: Answer? = null,
 ) {
 }
