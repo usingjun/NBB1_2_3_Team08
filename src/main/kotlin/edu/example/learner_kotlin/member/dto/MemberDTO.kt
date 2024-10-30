1976f7f554b9263668977d80195a07dd976f26a5
Si-rauis <kkyd7189@gmail.com>
2024-10-30 09:50:18 +0900

    Member Service 및 LoginDTO 마이그레이션

---
+++ src/main/kotlin/edu/example/learner_kotlin/member/dto/MemberDTO.kt
@@ -0,0 +1,36 @@
+package edu.example.learner_kotlin.member.dto
+
+import edu.example.learner_kotlin.member.entity.Member
+import jakarta.validation.constraints.NotNull
+import java.time.LocalDateTime
+
+data class MemberDTO(
+    var memberId: Long? = null,                  // 멤버 ID
+    @field:NotNull var email: String?,           // 이메일 (필수)
+    @field:NotNull var password: String?,        // 비밀번호 (필수)
+    @field:NotNull var nickname: String?,        // 닉네임 (필수)
+    var phoneNumber: String? = null,             // 전화번호 (선택적)
+    var profileImage: ByteArray? = null,         // 프로필 이미지 (선택적)
+    var imageType: String? = null,               // 이미지 타입 (선택적)
+    var introduction: String? = null,            // 자기소개 (선택적)
+    var createDate: LocalDateTime? = null        // 생성일 (선택적)
+){
+    constructor(member : Member) : this(
+        member.memberId,
+        member.email,
+        member.password,
+        member.nickname,
+        member.phoneNumber,
+        member.profileImage,
+        member.imageType,
+        member.introduction,
+        member.createDate
+    )
+
+    fun otherMember(): MemberDTO {
+        this.password = null
+        this.createDate = null
+
+        return this
+    }
+}
--- src/main/kotlin/edu/example/learner_kotlin/member/repository/MemberRepository.kt
+++ src/main/kotlin/edu/example/learner_kotlin/member/repository/MemberRepository.kt
@@ -1,11 +1,12 @@
 package edu.example.learner_kotlin.member.repository
 
 import edu.example.learner_kotlin.member.entity.Member
+import edu.example.learner_kotlin.member.repository.search.MemberSearch
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface MemberRepository : JpaRepository<Member, Long> {
+interface MemberRepository : JpaRepository<Member, Long> , MemberSearch {
     fun findByEmail(email: String?): Member
 
     fun findByPhoneNumber(phoneNumber: String?): List<Member?>
