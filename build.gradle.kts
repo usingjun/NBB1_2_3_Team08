80bb2984e4857901b7fcb6fcbaaba5dc57d500fd
usingjun <usingjun99@gmail.com>
2024-10-30 14:28:51 +0900

    Update build.gradle.kts 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    kotlin("kapt") version "1.9.25"
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("plugin.jpa") version "1.9.25"
+    kotlin("kapt") version "1.9.25"
 }
 
 group = "edu.example"
